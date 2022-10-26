1ba1b2ebc5d40aa8d35c08a307d8ecae7a9386d4
simon <simon.schoening@web.de>
2022-10-26 19:01:25 +0200

    bugs

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -39,7 +39,7 @@ type DockerWebhookRequest struct {
 	}
 }
 
-var ENV_REPO, ENV_TAG, ENV_SERVICE, ENV_WEBHOOK_TOKEN, ENV_SERVICESYNC_PORT string
+var ENV_REPO, ENV_TAG, ENV_SERVICE, ENV_WEBHOOK_TOKEN, ENV_SERVICESYNC_PORT, UPDATE_IMAGE string
 
 func main() {
 
@@ -50,9 +50,19 @@ func main() {
 	ENV_WEBHOOK_TOKEN = os.Getenv("ENV_WEBHOOK_TOKEN")
 	ENV_SERVICESYNC_PORT = os.Getenv("ENV_SERVICESYNC_PORT")
 
-	fmt.Println("Wachting for updates on service: " + ENV_SERVICE)
-	fmt.Println("Wachting for updates on image: " + ENV_REPO + ENV_TAG)
-	fmt.Println("Listening on :" + ENV_SERVICESYNC_PORT + "/webhooks/" + ENV_WEBHOOK_TOKEN)
+	// set image to update
+	if ENV_TAG != "" {
+		UPDATE_IMAGE = ENV_REPO + ":" + ENV_TAG
+	} else {
+		UPDATE_IMAGE = ENV_REPO
+	}
+
+	log.Printf("ENV_REPO: %v\n", ENV_REPO)
+	log.Printf("ENV_TAG: %v\n", ENV_TAG)
+	log.Printf("ENV_SERVICE: %v\n", ENV_SERVICE)
+	log.Printf("ENV_WEBHOOK_TOKEN: %v\n", ENV_WEBHOOK_TOKEN)
+	log.Printf("ENV_SERVICESYNC_PORT: %v\n", ENV_SERVICESYNC_PORT)
+	log.Printf("UPDATE_IMAGE: %v\n", UPDATE_IMAGE)
 
 	// start webserver
 	http.HandleFunc("/webhooks/"+ENV_WEBHOOK_TOKEN, syncDockerService)
@@ -60,44 +70,44 @@ func main() {
 }
 
 func syncDockerService(w http.ResponseWriter, req *http.Request) {
-	var params DockerWebhookRequest
 
+	// read params
+	var params DockerWebhookRequest
 	body, _ := ioutil.ReadAll(req.Body)
 	json.Unmarshal(body, &params)
-
-	fmt.Println("Incoming request found for image: " + params.Repository.Repo_name + ":" + params.Push_data.Tag)
+	log.Println("Incoming request found for image: " + params.Repository.Repo_name + ":" + params.Push_data.Tag)
 
 	if params.Repository.Repo_name == ENV_REPO && params.Push_data.Tag == ENV_TAG {
-		err := updateService(ENV_SERVICE, ENV_REPO+ENV_TAG)
+		err := updateService()
 		if err != nil {
-			http.Get(params.Callback_url)
+			log.Printf("err: %v\n", err)
 		}
-		fmt.Printf("err: %v\n", err)
+		http.Get(params.Callback_url)
 	} else {
-		fmt.Println("Image is not being watched:")
-		fmt.Println("IMAGE: " + params.Repository.Repo_name + " -> " + ENV_REPO)
-		fmt.Println("TAG: " + params.Push_data.Tag + " -> " + ENV_TAG)
+		log.Println("Image is not being watched:")
+		log.Println("IMAGE: " + params.Repository.Repo_name + " -> " + ENV_REPO)
+		log.Println("TAG: " + params.Push_data.Tag + " -> " + ENV_TAG)
 	}
 }
 
-func updateService(serviceID, imagename string) error {
+func updateService() error {
 
 	// start new docker client
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
-		fmt.Printf("cli: %v\n", cli)
+		log.Printf("cli: %v\n", cli)
 		return err
 	}
 
 	// scan running service
-	service, _, err := cli.ServiceInspectWithRaw(context.Background(), serviceID, types.ServiceInspectOptions{})
+	service, _, err := cli.ServiceInspectWithRaw(context.Background(), ENV_SERVICE, types.ServiceInspectOptions{})
 	if err != nil {
-		fmt.Printf("service: %v\n", service)
+		log.Printf("service: %v\n", service)
 		return err
 	}
 
 	// update service
-	service.Spec.TaskTemplate.ContainerSpec.Image = imagename
+	service.Spec.TaskTemplate.ContainerSpec.Image = UPDATE_IMAGE
 	service.Spec.TaskTemplate.ForceUpdate = uint64(time.Now().Unix())
 
 	// send update request to docker socket
@@ -109,7 +119,7 @@ func updateService(serviceID, imagename string) error {
 		types.ServiceUpdateOptions{})
 
 	if err != nil {
-		fmt.Printf("serviceResponse: %v\n", serviceResponse)
+		log.Printf("serviceResponse: %v\n", serviceResponse)
 		return err
 	}
 
